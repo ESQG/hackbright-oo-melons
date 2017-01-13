73f2ae0415d2c9f0dfa71a1a25bf915d903376f3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-12 16:26:24 -0800

    Refactored classes

---
+++ melons.py
@@ -0,0 +1,61 @@
+"""This file should have our order classes in it."""
+
+
+class AbstractMelonOrder(object):
+    """A melon order in an ideal world with no taxes."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes"""
+        self.shipped = False
+        self.species = species
+        self.qty = qty
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        if species == "christmas":
+            base_price *= 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    def __init__(self, species, qty):
+        super(DomesticMelonOrder, self).__init__(species, qty)
+        self.order_type = "domestic"
+        self.tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes"""
+
+        super(InternationalMelonOrder, self).__init__(species, qty)
+        self.country_code = country_code
+        self.order_type = "international"
+        self.tax = 0.17
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+    def get_total(self):
+        subtotal = super(InternationalMelonOrder, self).get_total()
+
+        if self.qty < 10:
+            flat_fee = 3
+            subtotal += flat_fee
+
+        return subtotal
